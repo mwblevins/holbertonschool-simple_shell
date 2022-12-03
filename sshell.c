686238077bd2ace236cd53a4086f276c94407d5d
Taypo87 <taylor.poczynek87@gmail.com>
2022-12-03 15:49:50 -0800

    fixed variable declaration, added comments

--- sshell.c
+++ sshell.c
@@ -7,33 +7,66 @@
  *
  */
 
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
-	char *buffer;
+	char *buffer = NULL;
 	size_t bufsize = 32;
-	size_t input;
+	int input;
 	pid_t childcheck;
-	char split[32];
-	int status;
+	char *split[32];
+	int status, i = 0;
 	char *tok;
 
+
 	if (argc == 1)
+		// check for interactive mode
 	{
 		while (1)
 		{
-			buffer = (char *)malloc(bufsize * sizeof(char));
+			// enter "infinite" interactive loop
+			
 			printf("($) ");
+
+			// get user commands
+
 			input = getline(&buffer, &bufsize, stdin);
-			childcheck = (fork);
-			tok = strtok(input, " ");
+
+			// fork process
+			printf("passed getline %d\n", input);
+
+			//childcheck = fork();
+
+			// double check how to hanadle \n
+
+			tok = strtok(buffer, " \n");
+			printf("pulled the first token %s\n", tok);
+			if (tok == NULL)
+			{
+				printf("failed to read command");
+				return (1);
+			}
+			split[i++] = tok;
+
+			// tokenize input and store it in an array
+
 			while (tok != NULL)
 			{
+				printf("%s\n", tok);
+				tok = strtok(NULL, " \n");
+				printf("%s\n", split[i]);
 				split[i++] = tok;
-				tok = strtok(NULL, " ");
+
 			}
+
+			// check if process is parent or child
+			childcheck = fork();
+
 			if (childcheck == 0)
-			       	execve(input, split, NULL);
+				// executes program called by user
+			       	execve(split[0], split, NULL);
+
 			wait(&status);
 		}
 	}
+	return (0);
 }
