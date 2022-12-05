7c107ab16ca32fb8fd7493303947dd87b86d83b8
Taypo87 <taylor.poczynek87@gmail.com>
2022-12-05 09:17:44 -0800

    cleaned up betty errors

--- sshell.c
+++ sshell.c
@@ -1,41 +1,27 @@
 #include "sshell.h"
 
 /**
- *
- *
- *
- *
+ * main - Shell, interactive or non interactive
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 1 if command fails
  */
 
 int main(int argc, char **argv)
 {
-	char *buffer = NULL;
 	size_t bufsize = 32;
-	int input;
+	int i = 0, status;
 	pid_t childcheck;
-	char *split[32];
-	int status, i = 0;
-	char *tok;
+	char *split[32], *tok, *buffer = NULL;
 
-
-	if (argc == 1)
-		// check for interactive mode
+	if (argc == 1 && argv != NULL)
 	{
 		while (1)
 		{
-			// enter "infinite" interactive loop
 			i = 0;
 			printf("($) ");
 
-			// get user commands
-
-			input = getline(&buffer, &bufsize, stdin);
-
-
-			//printf("passed getline %d\n", input);
-
-			// double check how to hanadle \n
-
+			getline(&buffer, &bufsize, stdin);
 			tok = strtok(buffer, " \n");
 			if (tok == NULL)
 			{
@@ -43,29 +29,17 @@ int main(int argc, char **argv)
 				return (1);
 			}
 			split[i++] = tok;
-
-			// tokenize input and store it in an array
-
 			while (tok != NULL && i < 32)
 			{
 				tok = strtok(NULL, " \n");
 				split[i++] = tok;
-
 			}
-
 			split[i - 1] = NULL;
-
-
-			// check if process is parent or child
 			childcheck = fork();
-
 			if (childcheck == 0)
 			{
-				// executes program called by user
-
 				execve(split[0], split, NULL);
 			}
-
 			wait(&status);
 			free(buffer);
 			buffer = NULL;
