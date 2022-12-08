22da49a16524d027f71368378b976c302b09999b
Taypo87 <taylor.poczynek87@gmail.com>
2022-12-08 12:49:20 -0800

    added non-interactive functionality

--- sshell.c
+++ sshell.c
@@ -8,10 +8,24 @@
 
 int main(int argc, char **argv)
 {
+	int i = 0;
+	char **args = NULL;
 
-	if (argc == 1 && argv)
+	if (argc == 1)
 	{
 		user_console();
+	}
+	if (argc > 1)
+	{
+		args = malloc(argc * sizeof(char *));
+		while (i < (argc - 1))
+		{
+			args[i] = argv[i + 1];
+			i++;
+		}
+
+		execve(args[0], args, NULL);
+		printf("./shell: No such file or directory\n");
 	}
 	return (0);
 }
